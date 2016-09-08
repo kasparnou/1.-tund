82096fb2646c37c882772621dbfe77251ef9ecbb
kaspnou <kaspnou@greeny.cs.tlu.ee>
2016-09-08 13:12:48 +0300

    first_file

---
+++ first_file.php
@@ -0,0 +1,12 @@
+<?php 
+
+	$firstName = "Kaspar";
+	$lastName = "Nõu";
+
+	// echo $firstName;
+	// echo $lastName;
+	
+	// Kahe stringi liitmiseks kasutan punkti
+	echo $firstName." ".$lastName;
+?>
+
